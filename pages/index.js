e0f898856e3c9e8d32a40c72215c2a9d4f3a5c93
Maximiler Arouca <maximiler@gmail.com>
2021-02-17 21:06:00 -0300

    Updates in code

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,18 @@
 export default function Home() {
   return (
-    <div className="flex flex-col lg:flex-row h-screen">
-      <main className="w-screen lg:w-4/7 h-screen flex flex-col lg:flex-row justify-center items-center lg:bg-bg-pattern bg-cover bg-center bg-no-repeat">
-        <div className="w-screen lg:w-1/2 h-screen lg:h-auto font-sans">
-          <img
-            src="images/logo.svg"
-            alt="Logo"
-            className="m-8 lg:ml-0 lg:mb-36 "
-          />
+    <div className="flex flex-col h-screen lg:flex-row">
+      <div className="w-screen h-screen font-sans lg:bg-bg-pattern bg-cover bg-center bg-no-repeat lg:flex lg:flex-row lg:justify-center lg:items-center">
+        <div className="lg:w-1/2">
+          <img src="images/logo.svg" alt="Logo" className="m-8 lg:ml-0 " />
           <img
             src="images/hero-mobile.jpg"
             alt="Hero image"
             className="w-full lg:hidden"
           />
-          <h1 className="mb-9 mt-12 lg:text-6xl text-4xl lg:w-1/2 text-center uppercase leading-tight text-dark tracking-widest font-semibold">
+          <h1 className="mb-9 mt-12 text-4xl text-center lg:text-left uppercase leading-tight text-dark tracking-widest font-semibold">
             <span className="text-light font-light">We're</span> coming soon
           </h1>
-          <p className="px-9 lg:px-0 text-light mb-9">
+          <p className="px-9 text-light mb-9 lg:px-0">
             Hello fellow shoppers! We're currently building our new fashion
             store. Add your email below to stay up-to-date with announcements
             and our launch deals.
@@ -32,8 +28,8 @@ export default function Home() {
             </button>
           </div>
         </div>
-      </main>
-      <div className="hidden lg:block bg-hero-background bg-cover bg-center bg-no-repeat w-3/5"></div>
+      </div>
+      <div className=" bg-hero-background bg-cover bg-center bg-no-repeat w-3/5"></div>
     </div>
   );
 }
